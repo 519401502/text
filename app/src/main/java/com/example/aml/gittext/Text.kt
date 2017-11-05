29ef85ae5bf4e1fc35654ddefcabd8d155715a8b
xvhuichuang <519401502@qq.com>
2017-11-05 23:34:15 +0800

    增加方法

--- app/src/main/java/com/example/aml/gittext/Text.kt
+++ app/src/main/java/com/example/aml/gittext/Text.kt
@@ -4,4 +4,10 @@ package com.example.aml.gittext
  * Created by 18624915319 on 2017/11/5.
  */
 class Text {
+
+
+    fun text(arg: String, argss: String): String{
+        var s = "测试"
+        return s
+    }
 }
