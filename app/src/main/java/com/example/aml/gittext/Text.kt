f3e614bae529537ca7c9bdbc4b83adbab7d944b0
xvhuichuang <519401502@qq.com>
2017-11-06 11:04:11 +0800

    增加log日志 - text分支

--- app/src/main/java/com/example/aml/gittext/Text.kt
+++ app/src/main/java/com/example/aml/gittext/Text.kt
@@ -1,5 +1,7 @@
 package com.example.aml.gittext
 
+import android.util.Log
+
 /**
  * Created by 18624915319 on 2017/11/5.
  */
@@ -8,6 +10,7 @@ class Text {
 
     fun text(arg: String, argss: String): String{
         var s = "测试"
+        Log.d("~", "测试")
         return s
     }
 }
